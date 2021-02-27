83a0d9f337b803b149ef4372d83905bab658eed4
sorinsi <sopromo@pm.me>
2021-02-27 22:40:34 +0100

    fix: installed_requires misconfiguration

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="djangorestframework-messagepack",
-    version="1.0.0",
+    version="1.0.1",
     url="https://github.com/sorinsi/djangorestframework-messagepack",
     license="MIT",
     description="Parser and renderer for MessagePack in Django Rest Framework",
     author="Sorin Cotoc",
     author_email="sopromo@protonmail.com",
     packages=find_packages(),
-    install_requires=open("requirements.txt").read().split("\n"),
+    install_requires=("msgpack>=1.0.0", "djangorestframework>=2.1.15", "Django>=1.8"),
     classifiers=[
         "Environment :: Web Environment",
         "Framework :: Django",
